46861bdc6ff2cfd97d4a9eaa5259b80b73428888
jacko.kaiser@gmail.com <jacko.kaiser@gmail.com@31637e1c-83f8-9215-a462-bdc96aa44127>
2012-10-07 18:42:38 +0000

    Le makefile devrait fonctionner correctement chez vous maintenant. $ make clean $ make $ make run
    Ajout de quelques photos trouvées sur internet dans Base_de_donnees/faceDatabase
    
    La reconnaissance faciale semble marcher avec ces photos.
    
    Jacques

--- trunk/Reconnaissance_Faciale/src/facialRecognition.cpp
+++ trunk/Reconnaissance_Faciale/src/facialRecognition.cpp
@@ -14,7 +14,7 @@ int main (int argc,char** argv)
   // Create a new Fisherfaces model and retain all available Fisherfaces,
   // this is the most common usage of this specific FaceRecognizer:
   //
-  Ptr<FaceRecognizer> model =  createFisherFaceRecognizer();
+  Ptr<FaceRecognizer> model =  createEigenFaceRecognizer();
 
   /////////////////////////////////////////////////// 
   /////////////////////////////////////////////////// train our FaceRecognizer
@@ -22,24 +22,33 @@ int main (int argc,char** argv)
   // holds images and labels
   vector<Mat> images;
   vector<int> labels;
-  // images for first person
-  images.push_back(imread("../Base_de_donnees/pic0.jpeg", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/pic1.jpeg", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/pic2.jpeg", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/pic3.jpeg", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/pic4.jpeg", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/pic5.jpeg", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  // This is the common interface to train all of the available cv::FaceRecognizer
-  // implementations:
-  //
+  // // images for first person
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s1/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s1/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s1/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s1/4.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s1/5.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s1/6.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s2/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s2/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s2/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s2/4.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s2/5.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+  images.push_back(imread("../Base_de_donnees/faceDatabase/s2/6.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+  // // This is the common interface to train all of the available cv::FaceRecognizer
+  // // implementations:
+  // //
   model->train(images, labels);
 
   /////////////////////////////////////////////////// 
   /////////////////////////////////////////////////// test if the following pic belongs
   /////////////////////////////////////////////////// 
 
-  Mat img = imread("../Base_de_donnees/pic6.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
-  int predicted_label=model->predict(img);
-  printf("label: %d\n",predicted_label);
+  Mat imgPerson1 = imread("../Base_de_donnees/faceDatabase/s1/7.pgm", CV_LOAD_IMAGE_GRAYSCALE);
+  Mat imgPerson2 = imread("../Base_de_donnees/faceDatabase/s2/7.pgm", CV_LOAD_IMAGE_GRAYSCALE);
+
+  printf("prediction sujet 1\nlabel: %d\nprediction sujet 2\nlabel: %d\n", model->predict(imgPerson1),model->predict(imgPerson2));
+  
   return 0;
 }
