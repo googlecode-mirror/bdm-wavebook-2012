9c61e67c432416a81509dc4dd28c9c6ebaace150
jacko.kaiser@gmail.com <jacko.kaiser@gmail.com@31637e1c-83f8-9215-a462-bdc96aa44127>
2012-10-18 16:01:53 +0000

    Ajout de la detection de visages

--- trunk/Reconnaissance_Faciale/src/facialRecognition.cpp
+++ trunk/Reconnaissance_Faciale/src/facialRecognition.cpp
@@ -102,12 +102,61 @@ void initImages()
 
 
 
+ /** Global variables */
+ String face_cascade_name = "OpenCV-2.4.2/data/haarcascades/haarcascade_frontalface_alt.xml";
+ String eyes_cascade_name = "OpenCV-2.4.2/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
+ CascadeClassifier face_cascade;
+ CascadeClassifier eyes_cascade;
+ RNG rng(12345);
+
+/** @function detectAndDisplay */
+void detectAndDisplay( Mat frame )
+{
+  std::vector<Rect> faces;
+  Mat frame_gray;
+
+  cvtColor( frame, frame_gray, CV_BGR2GRAY );
+  equalizeHist( frame_gray, frame_gray );
+
+  //-- Detect faces
+  face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+
+  for( int i = 0; i < faces.size(); i++ )
+    {
+      Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+      ellipse( frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
+
+      Mat faceROI = frame_gray( faces[i] );
+      imwrite("test.jpg" ,faceROI); // A JPG FILE IS BEING SAVED
+      std::vector<Rect> eyes;
+
+      //-- In each face, detect eyes
+      eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+
+      for( int j = 0; j < eyes.size(); j++ )
+	{
+	  Point center( faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5 );
+	  int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
+	  circle( frame, center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
+	}
+    }
+  //-- Show what you got
+  // imwrite("test.jpg" ,frame); // A JPG FILE IS BEING SAVED
+  // OF 6KB , BUT IT IS BLACK
+
+  // imshow( window_name, frame );
+}
+
 int main (int argc,char** argv)
 {
   // Create a new Fisherfaces model and retain all available Fisherfaces,
   // this is the most common usage of this specific FaceRecognizer:
   //
   Ptr<FaceRecognizer> model =  createEigenFaceRecognizer();
+
+  //-- 1. Load the cascades
+  if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+  if( !eyes_cascade.load( eyes_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
   /////////////////////////////////////////////////// 
   /////////////////////////////////////////////////// train our FaceRecognizer
@@ -127,6 +176,8 @@ int main (int argc,char** argv)
   Mat imgPerson1 = imread("../Base_de_donnees/faceDatabase/s1/7.pgm", CV_LOAD_IMAGE_GRAYSCALE);
   Mat imgPerson2 = imread("../Base_de_donnees/faceDatabase/s2/7.pgm", CV_LOAD_IMAGE_GRAYSCALE);
   Mat imgPerson3 = imread("../Base_de_donnees/faceDatabase/s3/1.pgm", CV_LOAD_IMAGE_GRAYSCALE);	
+  Mat imgPersonJack = imread("../Base_de_donnees/jacques/pic0.jpeg", CV_LOAD_IMAGE_COLOR);	
+  detectAndDisplay(imgPersonJack);
 
   printf("prediction sujet 1\nlabel: %d\nprediction sujet 2\nlabel: %d\n", model->predict(imgPerson1),model->predict(imgPerson2));
   printf("prediction sujet inconnu (3)\nlabel: %d\n", model->predict(imgPerson3));
