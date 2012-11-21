d3ccdb233d4c7efb04553e1cc78580148cc1069f
jacko.kaiser <jacko.kaiser@gmail.com>
2012-11-21 15:19:12 +0000

    The very only thing that remains to be done for facial recognition is to enlarge the ROI (region of interest) when detecting a face.
    to try the program:
    $ make run ARG=1 IMG=testDetect.jpg
    $ make run ARG=2 IMG=../Base_de_donnees/old/jacques/pic0.jpeg

--- Reconnaissance_Faciale/src/facialRecognition.cpp
+++ Reconnaissance_Faciale/src/facialRecognition.cpp
@@ -12,6 +12,15 @@
 #include <sstream>
 #include <string> 
 
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include "facialRecognition.h"
 #include "faceDetecter.h"
 #include "labelImage.h"
@@ -20,66 +29,62 @@
 using namespace cv;
 
 
-vector<LabelImage> trainingImages;
 
 std::vector<Mat> images;
 std::vector<int> labels;
 
 void initImages()
 {
-  // holds images and labels
-  // // images for first person
+  string filepath,filepathSub, baseName="../Base_de_donnees/upload/", dirSub="/profile/";
+  DIR *dp,*dpSub;
+  struct dirent *dirp,*dirpSub;
+  struct stat filestat,filestatSub;
+  Mat img;
+  int label;
 
-  //****************************************************
-  //****************************************************
-  // TODO: this function should read the whole database
-  //****************************************************
-  //****************************************************
-
-  string numDirectory;
-  string numImage;
-  string imgString;
-
-  for(int i = 1; i < 3; i++)
+  dp = opendir( baseName.c_str() );
+  if (dp == NULL)
     {
-
-      for(int j = 1; j < 7; j++)
-	{
-	  //numImage = "";
-	  std::stringstream outI;
-	  std::stringstream outJ;
-
-	  outI << i;
-	  numDirectory = outI.str();
-	  numDirectory += "/";
-
-	  outJ << j;
-	  numImage = outJ.str();
-	  numImage += ".pgm";
-
-	  printf("../Base_de_donnees/old/faceDatabase/s%s%s\n", numDirectory.c_str(), numImage.c_str());
-
-	  imgString = "../Base_de_donnees/old/faceDatabase/s"+ numDirectory + numImage;
-
-	  LabelImage baseImage = LabelImage(i, imread(imgString, CV_LOAD_IMAGE_GRAYSCALE));
-	  trainingImages.push_back(baseImage);
-	}
+      std::cout << "Error opening " << baseName << std::endl;
+      exit(1);
     }
 
-  //trainingImages
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s1/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s1/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s1/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s1/4.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s1/5.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s1/6.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(1);
+  while ((dirp = readdir( dp )))
+    {
+      // we are reading dirs Base_de_donnees/upload/
+      filepath = baseName  + dirp->d_name;
 
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s2/1.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s2/2.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s2/3.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s2/4.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s2/5.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
-  images.push_back(imread("../Base_de_donnees/old/faceDatabase/s2/6.pgm", CV_LOAD_IMAGE_GRAYSCALE)); labels.push_back(2);
+      stat( filepath.c_str(), &filestat );
+      if (S_ISDIR( filestat.st_mode ) && (isdigit(dirp->d_name[0])))
+	{
+	  std::cout<<"Reading dir "<<filepath<<std::endl;
+	  // we are reading dirs Base_de_donnees/upload/[ID]
+	  // if we have a dir that starts with a digit, we take label	
+	  label=atoi(dirp->d_name);
+	  dpSub = opendir( (filepath+dirSub).c_str() );
+	  if (dpSub == NULL)
+	    {
+	      std::cout << "Error opening " << baseName << std::endl;
+	      exit(1);
+	    }
+	  while ((dirpSub = readdir( dpSub )))
+	    {
+	      // we are reading dirs Base_de_donnees/upload/[ID]/profile/
+	      filepathSub = filepath + dirSub  + dirpSub->d_name;
+
+	      // If the file is a directory (or is in some way invalid) we'll skip it
+	      if (stat( filepathSub.c_str(), &filestatSub )) continue;
+	      if (S_ISDIR( filestatSub.st_mode ))         continue;
+	      std::cout<<"\tReading file "<<filepathSub<<std::endl;
+
+	      // we assume the file is an image: push back in the vector, and push back a label
+	      images.push_back(imread(filepathSub, CV_LOAD_IMAGE_GRAYSCALE));
+	      labels.push_back(label);
+	    }
+	  closedir( dpSub );
+	}
+    }
+  closedir( dp );
 }
 
 
@@ -87,53 +92,61 @@ void initImages()
 
 int whois (Mat personToPredict)
 {
+
+  /////////////////////////////////////////////////// 
+  /////////////////////////////////////////////////// Create a new Fisherfaces model 
+  ///////////////////////////////////////////////////
   Ptr<FaceRecognizer> model =  createEigenFaceRecognizer();
-  //Create a new Fisherfaces model and retain all available Fisherfaces,
-  //this is the most common usage of this specific FaceRecognizer:
   
+
+
+  /////////////////////////////////////////////////// 
+  /////////////////////////////////////////////////// fill our vectors with database images/labels
+  ///////////////////////////////////////////////////
+  initImages();
+
 
   /////////////////////////////////////////////////// 
   /////////////////////////////////////////////////// train our FaceRecognizer
   ///////////////////////////////////////////////////
-
-  initImages();
-
-  // // This is the common interface to train all of the available cv::FaceRecognizer
-  // // implementations:
-  // //
   model->train(images, labels);
 
   /////////////////////////////////////////////////// 
   /////////////////////////////////////////////////// test if the following pic belongs
   /////////////////////////////////////////////////// 
-
   return model->predict(personToPredict);
 }
 
 
 int main (int argc,char** argv)
 {
-  if(argc<1)
+  if(argc<2)
     {
-      printf("Please specify an argument:\n1 for face recognition\n2 reframe image\n");
+      printf("Please specify an arguments:\n1 {pathToAnImage} for face recognition\n2 {pathToAnImage} reframe image\n");
       exit(-1);
     }
   switch(atoi(argv[1]))
     {
     case (1):
       {
-	Mat imgPersonJack = imread("../Base_de_donnees/old/jacques/pic1.jpeg", CV_LOAD_IMAGE_COLOR);	
-	// Mat imgPerson = imread(argv[2], CV_LOAD_IMAGE_COLOR);	
-	return whois(imgPersonJack);
+	Mat imgPerson = imread(argv[2], CV_LOAD_IMAGE_COLOR);	
+	initImages();
+	return whois(imgPerson);
       }
       break;
     case (2):
       {
+	std::string fileOut(argv[2]);
+	size_t found=fileOut.find_last_of("/");
+	// case we have a path
+	if (found!=string::npos)
+	  fileOut=fileOut.substr(found+1,fileOut.length()-1);
+	// if there's no path, it's just a filename
+	fileOut="R"+fileOut;
 	FaceDetecter detecter;
-	Mat imgPersonJack = imread("../Base_de_donnees/old/jacques/pic1.jpeg", CV_LOAD_IMAGE_COLOR);	
-	// Mat imgPerson = imread(argv[2], CV_LOAD_IMAGE_COLOR);	
-	Mat imgPersonJackReframed;
-	int retDetect=detecter.detectAndReframe(imgPersonJack,imgPersonJackReframed);
+	Mat imgPerson = imread(argv[2], CV_LOAD_IMAGE_COLOR);	
+	Mat imgPersonReframed;
+	int retDetect=detecter.detectAndReframe(imgPerson,imgPersonReframed);
 	switch(retDetect)
 	  {
 	  case MANY_FACES_FOUND:
@@ -152,9 +165,8 @@ int main (int argc,char** argv)
 	    }
 	    break;
 	  }
-	const char outputFile[]="testDetect.jpg";
-	imwrite(outputFile,imgPersonJackReframed);
-	printf("image written to %s\n",outputFile);
+	imwrite(fileOut.c_str(),imgPersonReframed);
+	printf("image written to %s\n",fileOut.c_str());
       }
     }
   
