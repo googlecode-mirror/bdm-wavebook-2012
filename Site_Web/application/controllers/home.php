61e108594d242033ca53edcde465cd4991f4ec7e
jonathanhaehnel <jonathanhaehnel@gmail.com>
2012-12-19 15:07:39 +0000

    Mise en place du backend php pour l'upload des images capturées par la webcam, webcam fonctionnelle sur toutes les pages (inscription, login, change avatar)

--- Site_Web/application/controllers/home.php
+++ Site_Web/application/controllers/home.php
@@ -84,6 +84,81 @@ class Home extends GuestController
 	}
 	
 	/**
+	 * Enregistrement d'un nouvel utilisateur avec une webcam
+	 */
+	public function register_validation_with_cam()
+	{
+		$this->load->library('form_validation');
+		$this->load->helper('form');
+		$this->form_validation->set_error_delimiters('<div class="alert alert-error"><button type="button" class="close" data-dismiss="alert">×</button>', '</div>');
+			
+		//mise en place des regles
+		$this->form_validation->set_rules('name', 'Nom', 'required|encode_php_tags|htmlspecialchars|trim|xss_clean|max_length[20]');
+		$this->form_validation->set_rules('vorname', 'Prénom', 'required|encode_php_tags|htmlspecialchars|trim|xss_clean|max_length[20]');
+		$this->form_validation->set_rules('password', 'Mot de passe', 'required|alpha|max_length[20]');
+		$this->form_validation->set_rules('email', 'Email', 'required|valid_email|max_length[150]');
+		$this->form_validation->set_rules('sexe', 'Sexe', 'required|matches[sexe]');
+		$this->form_validation->set_rules('cgu', 'Conditions générales d\'utilisation', 'required');
+		$this->form_validation->set_rules('url_capture', 'Photo(s) capturée(s)', 'required');
+		
+		
+		if ($this->form_validation->run() == FALSE) // echec
+		{
+			//on affiche le formulaire
+			$this->register();
+		}
+		else // reussite
+		{
+			//creation de l'utilisateur
+			$user = new User();
+			$user->name = $this->input->post('name');
+			$user->vorname = $this->input->post('vorname');
+			$user->password = strtolower($this->input->post('password'));
+			$user->email = $this->input->post('email');
+			if($this->input->post('sexe') == "Femme") $user->sex = 0;
+			else $user->sex = 1;
+			
+			//sauvegarde de l'utilisateur
+			$user->save();
+			
+			// decompossition des images capturées
+			$list_capture = explode(',', $this->input->post('url_capture'),4);
+			
+			//upload de l'avatar
+			$avatar_up = new Upload();
+			if($avatar_up->upload_avatar_capture($user->id, $list_capture))
+			{
+				//Verification de la conformité de l'avatar
+				// appel FaceDetection...
+
+				//Enregistrement dans la BDD de chaque avatar
+				//print_r($avatar_up->files_uploaded);
+				for($i = 0; $i < count($avatar_up->files_uploaded); $i++)
+				{
+					$new_avatar = new Avatar();
+					$new_avatar->url = $avatar_up->files_uploaded[$i][0];
+					$new_avatar->id_user = $user->id;
+					$new_avatar->save();
+				}
+				
+				//notification
+				$this->session->set_userdata('notif_ok','<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button><strong>Inscription réussite!</strong> Vous pouvez maintenant vous connecter au site !</div>');
+				
+				//redirection sur l'accueil
+				redirect('home/index','refresh');
+			}
+			else
+			{
+				//on supprime l'utilisateur et son espace de travail
+				$user->delete();
+				
+				//on affiche le formulaire
+				$this->register();
+			}
+		}
+	}
+	
+	/**
 	 * Page d'inscription d'un nouvel user
 	 */
 	public function register()
